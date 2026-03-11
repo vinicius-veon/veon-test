658c564be18819068d3ba071c50fb4d50f9de960
Vinícius de Paula <manoseilacara123@gmail.com>
2026-03-10 21:27:13 -0300

    feat: create user module

---
+++ src/users/user.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common'
+import { UserController } from './controllers/user.controller'
+import { UserService } from './service/user.service'
+import { PrismaService } from './service/prisma.service'
+import { PrismaUserRepository } from './repository/prisma.user.repository'
+import { UserRepository } from './repository/user.repository'
+
+@Module({
+  controllers: [UserController],
+  providers: [
+    PrismaService,
+    UserService,
+    {
+      provide: UserRepository,
+      useClass: PrismaUserRepository,
+    },
+  ],
+})
+export class UserModule {}
