8a0a64cac9209bbb2ff68493d5cffc1100bdbc93
Vinícius de Paula <manoseilacara123@gmail.com>
2026-03-10 22:44:40 -0300

    fix: add whitelist = true to validation pipe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { HttpExceptionFilter } from './common/filters/http-exceptions-filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
   app.useGlobalFilters(new HttpExceptionFilter())
 
   await app.listen(process.env.PORT ?? 3000)
