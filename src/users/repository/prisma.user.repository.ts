e3bd28031bde6d1957ad3db023b8dc10ce735059
Vinícius de Paula <manoseilacara123@gmail.com>
2026-03-10 21:26:14 -0300

    feat: create user repository interface and prisma user repository

---
+++ src/users/repository/prisma.user.repository.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common'
+import { CreateUserDto } from '../domain/dto/create-user.dto'
+import { User } from '../domain/user.entity'
+import { UserMapper } from '../mappers/user.mapper'
+import { PrismaService } from '../service/prisma.service'
+import { UserRepository } from './user.repository'
+import { UpdateUserDto } from '../domain/dto/update-user.dto'
+
+@Injectable()
+export class PrismaUserRepository implements UserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDto): Promise<void> {
+    await this.prisma.user.create({ data })
+  }
+
+  async findAll(): Promise<User[]> {
+    const prismaUsers = await this.prisma.user.findMany()
+    return prismaUsers.map((user) => UserMapper.toDomain(user))
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const prismaUser = await this.prisma.user.findUnique({ where: { id } })
+    return prismaUser ? UserMapper.toDomain(prismaUser) : null
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const prismaUser = await this.prisma.user.findUnique({ where: { email } })
+    return prismaUser ? UserMapper.toDomain(prismaUser) : null
+  }
+
+  async update(id: string, data: UpdateUserDto): Promise<void> {
+    await this.prisma.user.update({
+      where: { id },
+      data,
+    })
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.user.delete({ where: { id } })
+  }
+}
---
+++ src/users/repository/user.repository.ts
@@ -0,0 +1,12 @@
+import { CreateUserDto } from '../domain/dto/create-user.dto'
+import { UpdateUserDto } from '../domain/dto/update-user.dto'
+import { User } from '../domain/user.entity'
+
+export abstract class UserRepository {
+  abstract create(data: CreateUserDto): Promise<void>
+  abstract findAll(): Promise<User[]>
+  abstract findById(id: string): Promise<User | null>
+  abstract findByEmail(email: string): Promise<User | null>
+  abstract update(id: string, user: UpdateUserDto): Promise<void>
+  abstract delete(id: string): Promise<void>
+}
